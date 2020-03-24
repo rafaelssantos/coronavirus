4cb347c22298d68ba4210ccdfc8cbacc43be7e5e
Rafael Silva Santos <rafael.silva.sts@gmail.com>
2020-03-24 03:47:19 -0300

    Update content.css, header.css, and header.php

--- html/header.php
+++ html/header.php
@@ -17,6 +17,7 @@
 			<li><a id='mn-news' href="noticias/">Notícias</a></li>
 			<li><a id='mn-services' href="funcionamento-de-servicos.php">Funcionamento de serviços</a></li>
 			<li><a id='mn-orientations' href="orientacoes.php">Orientações</a></li>
+			<li><a id='mn-questions' href="pergundas-frequentes.php">Perguntas frequentes</a></li>
 			<li><a id='mn-contact' href="contato.php">Contato</a></li>
 		</div>
 	</nav>
