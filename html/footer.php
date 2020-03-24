05a4bb2df4599b24db825608cb0e58e743e290bd
Rafael Silva Santos <rafael.silva.sts@gmail.com>
2020-03-24 04:18:13 -0300

    Correções de estilo.

--- html/footer.php
+++ html/footer.php
@@ -1,6 +1,5 @@
 <footer class='footer'>
 	<div class='footer-container'>
-
 		<div class='copyright-container'>
 				<p><a href='http://www.quata.sp.gov.br/'>Prefeitura Municipal de Quatá</a> ─ Rua General Marcondes Salgado, nº 332. (18) 3366-9500 | prefeituraquata@quata.sp.gov.br</p>
 		</div>
