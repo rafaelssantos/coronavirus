e5c8ef291f1095ab21bd08d2168391796e743b73
Rafael Silva Santos <rafael.silva.sts@gmail.com>
2020-04-03 09:11:13 -0300

    Decreto 4109 de 01 de abril de 2020.

---
+++ legislacao/inclusao-correspondentes-bancarios.php
@@ -0,0 +1,86 @@
+<?php
+include_once('../conf/conf.php');
+?>
+
+
+<?php
+require_once(ROOT_DIR . '/classes/NewsManager.php');
+$contact_url = BASE_URL_DIR . "contato.php";
+?>
+
+
+<!DOCTYPE html>
+<html lang="pt">
+
+<head>
+	<meta charset='UTF-8'>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<title>Legislação ─ CGC da Prefeitura Municipal de Quatá-SP</title>
+
+	<base href=<?php echo BASE_URL_DIR; ?>>
+
+
+	<link rel='shortcut icon' href='img/logo.ico'>
+
+	<link rel='stylesheet' type='text/css' href='css/reset.css'>
+	<link rel='stylesheet' type='text/css' href='css/html.css'>
+	<link rel='stylesheet' type='text/css' href='css/header.css'>
+	<link rel='stylesheet' type='text/css' href='css/content.css'>
+	<link rel='stylesheet' type='text/css' href='css/footer.css'>
+	<link rel='stylesheet' type='text/css' href='css/news.css'>
+
+	<script src="javascript/lib/jquery-3.4.1.min.js"></script>
+	<script src="https://kit.fontawesome.com/fa170b8b16.js" crossorigin="anonymous"></script>
+</head>
+
+
+
+<body>
+	<?php include(HTDOCS_DIR . '/html/header.php'); ?>
+
+	<script>
+		$("#mn-news").addClass("current-menu-item");
+	</script>
+
+
+
+	<div class='content'>
+		<span class='date'>01 de abril de 2020</span>
+		<h1>Decreto de Inclusão de Correspondentes Bancários como Serviços Essenciais, altera Decreto 4.103 ─ DECRETO Nº 4.109</h1>
+		<span class='author'>O texto reproduzido tem caráter informativo e não substitui o <a href=""></a>publicado no diário oficial do município</span>
+
+		<p><strong>"ALTERA O DECRETO MUNICIPAL Nº. 4.103 DE 21/03/2020, COM REDAÇÃO DADA PELO DECRETO MUNICIPAL N° 4.107 DE 26/03/2030, PARA INCLUIR OS POSTOS DE CORRESPONDENTES BANCÁRIOS COMO PRESTADORES DE SERVIÇOS ESSENCIAIS À POPULAÇÃO E DÁ OUTRAS PROVIDÊNCIAS".</strong></p>
+
+        <p><strong>MARCIO BIDOIA</strong>, Prefeito em Exercício do Município de Quatá, Estado de São Paulo, no uso e suas atribuições legais;</p>
+
+        <p><strong>CONSIDERANDO</strong> o disposto no Decreto Municipal nº 4.103, de 21 de março de 2020, que decretou estado de calamidade pública no Município de Quatá;</p>
+
+        <p><strong>CONSIDERANDO</strong> o disposto no Decreto Municipal nº 4.107, de 26 de março de 2020, que decretou estado de calamidade pública no Município de Quatá;</p>
+
+        <p><strong>CONSIDERANDO</strong> a necessidade de se alinhar as determinações do Governo do Estado de São Paulo com as determinações e as especificidades do Poder Público Municipal;</p>
+
+        <p><strong>DECRETA:</strong></p>
+
+        <p><strong>Artigo 1º</strong> – O artigo 3º, parágrafo 4º, do Decreto nº. 4.103, de 21 de março de 2020, com redação dada pelo Decreto nº. 4107, de 26 de março de 2020, passa a vigorar com a seguinte redação:</p>
+
+        <p><em>"Artigo 3º ..................................................................................</em></p>
+        <p><em>§ 4º – Ficam excetuadas da suspensão determinada por este Decreto, o funcionamento das instituições ou agências bancárias, postos de correspondentes bancários e as agências dos correios, por serem consideradas serviço essencial à população e deverão adotar as seguintes providências:</em></p>
+
+        <p><em>"I- .........................................................................................</em></p>
+        <p><em>"II- ........................................................................................</em></p>
+        <p><em>"III-........................................................................................</em></p>
+        <p><em>§ 5º – Os postos de correspondentes bancários deverão funcionar, exclusivamente, para o cumprimento do objeto previsto em contrato firmado com a instituição financeira contratante, no horário das 10:00 às 14:00H, vedado o exercício do comércio para produtos ou serviços diversos da atividade bancária autorizada.</em></p>
+
+        <p><strong>Artigo 2º</strong> – Ficam ratificados os demais termos do Decreto Municipal nº 4.103, de 21 de março de 2020 e Decreto 4.107, de 26 de março de 2020, sendo que, as medidas previstas poderão ser reavaliadas a qualquer momento.</p>
+
+        <p><strong>Artigo 3º</strong> – Este Decreto entra em vigor na data de sua publicação.</p>
+
+        <h2 class='inside-header'>Fontes</h2>
+		<ul>
+			<li>PREFEITURA MUNICIPAL DE QUATÁ. <em>Diário Oficial do Município</em>. Data da publicação: 02 de abril de 2020. Disponível em: <a href="https://dosp.com.br/exibe_do.php?i=MTAyNjMz">https://dosp.com.br/exibe_do.php?i=MTAyNjMz</a></li>
+		</ul>
+	</div>
+
+	<?php include(HTDOCS_DIR . '/html/footer.php'); ?>
+</body>
