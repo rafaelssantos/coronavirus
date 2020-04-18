e129ad22a1cc1af5e9854c3b8d8173c1683551a3
Rafael Silva Santos <rafael.silva.sts@gmail.com>
2020-04-18 20:55:58 -0300

    Atualização de casos.

--- index.php
+++ index.php
@@ -45,7 +45,7 @@ $contact_url = BASE_URL_DIR . "contato.php";
 
 	<div class='content'>
 		<div class='update-panel-container'>
-			<span>Última atualização em 15 de abril de 2020 às 20:00.</span>
+			<span>Última atualização em 18 de abril de 2020 às 20:55.</span>
 		</div>
 
 		<div class='cases-panel-container'>
@@ -53,13 +53,13 @@ $contact_url = BASE_URL_DIR . "contato.php";
 				<span class='cases-label'>*Suspeitos em monitoramento</span><span class='cases-counter'>008</span>
 			</div>
 			<div class='cases-label-container' id='label-suspeitos-em-investigacao'>
-				<span class='cases-label'>**Suspeitos em investigação</span><span class='cases-counter'>009</span>
+				<span class='cases-label'>**Suspeitos em investigação</span><span class='cases-counter'>004</span>
 			</div>
 			<div class='cases-label-container' id='label-recuperados'>
-				<span class='cases-label'>***Recuperados</span><span class='cases-counter'>026</span>
+				<span class='cases-label'>***Recuperados</span><span class='cases-counter'>030</span>
 			</div>
 			<div class='cases-label-container' id='label-descartados-por-exame'>
-				<span class='cases-label'>Descartados por exame</span><span class='cases-counter'>000</span>
+				<span class='cases-label'>Descartados por exame</span><span class='cases-counter'>005</span>
 			</div>
 			<div class='cases-label-container' id='label-suspeitos-em-internacao-hospitalar'>
 				<span class='cases-label'>Internação Hospitalar</span><span class='cases-counter'>002</span>
@@ -100,41 +100,41 @@ $contact_url = BASE_URL_DIR . "contato.php";
 	new Chart(document.getElementById("cases-chart"), {
 		type: 'line',
 		data: {
-			labels: ['21/03', '22/03', '23/03', '24/03', '25/03', '26/03', '27/03', '28/03', '29/03', '30/03', '31/03', '01/04', '02/04', '03/04', '04/04', '05/04', '06/04', '07/04', '08/04', '09/04', '10/04', '11/04', '12/04', '13/04', '14/04', '15/04'],
+			labels: ['21/03', '22/03', '23/03', '24/03', '25/03', '26/03', '27/03', '28/03', '29/03', '30/03', '31/03', '01/04', '02/04', '03/04', '04/04', '05/04', '06/04', '07/04', '08/04', '09/04', '10/04', '11/04', '12/04', '13/04', '14/04', '15/04', '16/04', '17/04', '18/04'],
 			datasets: [{
-				data: [04, 04, 06, 06, 12, 12, 15, 15, 15, 14, 17, 14, 15, 14, 14, 10, 10, 12, 13, 14, 12, 11, 12, 10, 09, 08],
+				data: [04, 04, 06, 06, 12, 12, 15, 15, 15, 14, 17, 14, 15, 14, 14, 10, 10, 12, 13, 14, 12, 11, 12, 10, 09, 08, 07, 09, 08],
 				label: "Suspeitos em monitoramento",
 				borderColor: "#FFFF00",
 				fill: false
 			},
 			{
-				data: [00, 01, 04, 04, 04, 04, 04, 04, 04, 05, 05, 05, 06, 06, 06, 06, 06, 06, 06, 07, 07, 07, 07, 08, 09, 09],
+				data: [00, 01, 04, 04, 04, 04, 04, 04, 04, 05, 05, 05, 06, 06, 06, 06, 06, 06, 06, 07, 07, 07, 07, 08, 09, 09, 09, 04, 04],
 				label: "Suspeitos em investigação",
 				borderColor: "#FFC800",
 				fill: false
 			},
 			{
-				data: [00, 00, 00, 00, 00, 00, 00, 00, 01, 01, 03, 03, 04, 05, 06, 16, 16, 17, 18, 18, 20, 20, 20, 23, 26, 28],
+				data: [00, 00, 00, 00, 00, 00, 00, 00, 01, 01, 03, 03, 04, 05, 06, 16, 16, 17, 18, 18, 20, 20, 20, 23, 26, 28, 29, 29, 30],
 				label: "Recuperados",
 				borderColor: "#0000FF",
 				fill: false
 			}, {
-				data: [00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00],
+				data: [00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 05, 05],
 				label: "Descartados por exame",
 				borderColor: "#008000",
 				fill: false
 			}, {
-				data: [00, 00, 00, 00, 00, 00, 00, 00, 00, 01, 01, 01, 01, 01, 01, 01, 01, 01, 01, 02, 02, 02, 02, 02, 02, 02],
+				data: [00, 00, 00, 00, 00, 00, 00, 00, 00, 01, 01, 01, 01, 01, 01, 01, 01, 01, 01, 02, 02, 02, 02, 02, 02, 02, 02, 02, 02],
 				label: "Internação Hospitalar",
 				borderColor: "#8B0000",
 				fill: false
 			}, {
-				data: [00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00],
+				data: [00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00],
 				label: "Confirmados por exame",
 				borderColor: "#FF0000",
 				fill: false
 			}, {
-				data: [00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00],
+				data: [00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00],
 				label: "Óbitos",
 				borderColor: "#000000",
 				fill: false
