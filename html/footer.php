cae9346693ff0953778d19cc0cb5bc6243c224a6
Rafael Silva Santos <rafael.silva.sts@gmail.com>
2021-04-28 09:22:52 -0300

    Atualização de estilo.

--- html/footer.php
+++ html/footer.php
@@ -1,7 +1,7 @@
 <footer class='footer'>
 	<div class='footer-container'>
 		<div class='copyright-container'>
-			<p>© 2020 ─ Setor de Tecnologia da Informação (STI) ─ <a href='http://www.quata.sp.gov.br/'>Prefeitura Municipal de Quatá-SP</a></p>
+			<p>© 2021 ─ Setor de Tecnologia da Informação (STI) ─ <a href='http://www.quata.sp.gov.br/'>Prefeitura Municipal de Quatá-SP</a></p>
 		</div>
 		<div class='social-container'>
 			<a id='facebook-logo' title="Siga nossa página no Facebook" href="https://www.facebook.com/prefquata/" target="_blank"><i class="fab fa-facebook-f"></i></a>
