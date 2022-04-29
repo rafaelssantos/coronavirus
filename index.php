a8deba5e0f8cfce5101f327f022ce0f1971174a0
Gabriel Mossini <ga.mossini@gmail.com>
2022-04-29 09:57:36 -0300

    "Atualização de Casos 29/04/22 - Correção de Doses Aplicadas"

--- index.php
+++ index.php
@@ -129,7 +129,7 @@ $contact_url = BASE_URL_DIR . "contato.php";
 						Doses aplicadas
 					</div>
 					<div class="container font-bold h2">
-						32145
+						32183
 					</div>
 				</div>
 				<div class="col-12 bg-gray-1 rounded shadow text-black">
